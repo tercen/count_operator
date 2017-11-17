e8c05bac71f12e9e247c8e9311a21625e536cc1d
amaurel <alexandre.maurel@gmail.com>
2017-11-17 13:04:44 +0100

    init

---
+++ main.R
@@ -0,0 +1,9 @@
+library(tercen)
+library(dplyr)
+
+(ctx = tercenCtx())  %>% 
+  select(.y, .ci, .ri) %>% 
+  group_by(.ci, .ri) %>%
+  summarise(count = length(.y)) %>%
+  ctx$addNamespace() %>%
+  ctx$save()
