9e7d3efc9b0e9e1f6761f614a988564fbbdc4675
lucas501 <lucas.lelann@gmail.com>
2022-08-30 08:36:09 +0000

    Add tests and populate workflows

--- main.R
+++ main.R
@@ -1,9 +1,13 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
+ctx <- tercenCtx()
+
+df.out<-ctx %>% 
   select(.y, .ci, .ri) %>% 
   group_by(.ci, .ri) %>%
   summarise(count = as.double(length(.y))) %>%
-  ctx$addNamespace() %>%
+  ctx$addNamespace() 
+
+df.out %>%
   ctx$save()
